fbd2e0afc42c35f9ab569e7949f84f7959457e19
이름 <cho50058@gmail.com>
2024-05-28 03:37:24 +0900

    Init: "생성 stone_game 프로젝트"
    프로잭트 생성 후 메인method  정상 작동 확인.

---
+++ src/Game.java
@@ -0,0 +1,15 @@
+public class Game {
+
+    public static void main(String[] args) {
+        gameStart();
+        gameEnd();
+    }
+
+    private static void gameStart() {
+        System.out.println("==Start Game==");
+    }
+
+    private static void gameEnd() {
+        System.out.println("==End Game==");
+    }
+}
